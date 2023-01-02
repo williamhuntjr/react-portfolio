452a47757107c70d338815dbe7ed0e633f63b8f7
William Hunt Jr <contact@williamhuntjr.com>
2023-01-02 13:44:51 -0500

    updated config and removed Jenkinsfile

--- src/common/config.ts
+++ src/common/config.ts
@@ -3,5 +3,5 @@ export interface IGlobalConstants {
 }
 
 export const appConfig: IGlobalConstants = {
-  API: process.env.REACT_APP_API_URL || 'https://api.huntdesignco.com',
+  API: process.env.REACT_APP_API_URL || 'https://api.williamhuntjr.com',
 }
